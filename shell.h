28af2edc614bba57d95f7b4a340aea2d60f0d7a8
Allan Kevin <134234752+Aellun@users.noreply.github.com>
2023-08-23 18:46:28 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -139,8 +139,8 @@ char *_strcat(char *, char *);
 /* Declarations of functions defined in toem_string1.c */
 char *_strcpy(char *, char *);
 char *_strdup(const char *);
-void _puts(const char *str)
-int _putchar(char);
+void _puts(const char *str);
+int _putchar(char c);
 
 /* Declarations of functions defined in toem_exits.c */
 char *_strncpy(char *, char *, int);
