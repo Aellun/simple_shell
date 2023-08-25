8b9b797d5b2f3ce75f63e53f597791f9d1b99f27
Aellun <okello.k.otieno@gmail.com>
2023-08-25 11:43:27 -0700

    updated files and compiled

--- vars.c
+++ vars.c
@@ -1,15 +1,16 @@
 #include "shell.h"
 
 /**
- * is_chain - determine if current char in buffer is a chain delimeter
+ * is_chain - determine if current char in buffer is a chain delimiter
  * @info: ptr to the to struct
  * @buf: ptr to array buf
  * @p: ptr to size variable -t
- * Return: 1 if chain delimeter, 0 otherwise
+ * Return: 1 if chain delimiter, 0 otherwise
  */
 int is_chain(info_t *info, char *buf, size_t *p)
 {
 	size_t d = *p;
+
 	if (buf[d] == '|' && buf[d + 1] == '|')
 	{
 		buf[d] = 0;
@@ -22,9 +23,9 @@ int is_chain(info_t *info, char *buf, size_t *p)
 		d++;
 		info->cmd_buf_type = CMD_AND;
 	}
-	else if (buf[d] == ';') /*end of this command */
+	else if (buf[d] == ';') /* end of this command */
 	{
-		buf[d] = 0; /*eplace semicolon with null */
+		buf[d] = 0; /* replace semicolon with null */
 		info->cmd_buf_type = CMD_CHAIN;
 	}
 	else
@@ -32,6 +33,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
 	*p = d;
 	return (1);
 }
+
 /**
  * check_chain - determine if should chaining based on the status last
  * @info: the ptr struct
@@ -44,6 +46,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 {
 	size_t d = *p;
+
 	if (info->cmd_buf_type == CMD_AND)
 	{
 		if (info->status)
@@ -64,7 +67,7 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 }
 
 /**
- * replace_alias - put in plsce an aliases in the tokenized str
+ * replace_alias - put in place an aliases in the tokenized str
  * @info: the ptr struct
  * Return: 1 if replaced, 0 otherwise
  */
@@ -73,6 +76,7 @@ int replace_alias(info_t *info)
 	int l;
 	list_t *node;
 	char *p;
+
 	for (l = 0; l < 10; l++)
 	{
 		node = node_starts_with(info->alias, info->argv[0], '=');
@@ -88,52 +92,53 @@ int replace_alias(info_t *info)
 		info->argv[0] = p;
 	}
 	return (1);
-	/**
-	 * replace_vars - replaces vars in the tokenized str
-	 * @info: the ptr struct
-	 * Return: 1 if replaced, 0 otherwise
-	 */
-	int replace_vars(info_t *info)
-	{
-		int l = 0;
-		list_t *node;
+}
 
-		for (l = 0; info->argv[l]; l++)
-		{
-			if (info->argv[l][0] != '$' || !info->argv[l][1])
-				continue;
-			if (!_strcmp(info->argv[l], "$?"))
-			{
-				replace_string(&(info->argv[l]),
-						_strdup(convert_number(info->status, 10, 0)));
-				continue;
-			}
-			if (!_strcmp(info->argv[l], "$$"))
-			{
-				replace_string(&(info->argv[l]),
-						_strdup(convert_number(getpid(), 10, 0)));
-				continue;
-			}
-			node = node_starts_with(info->env, &info->argv[l][1], '=');
-			if (node)
-			{
-				replace_string(&(info->argv[l]),
-						_strdup(_strchr(node->str, '=') + 1));
-				continue;
-			}
-			replace_string(&info->argv[l], _strdup(""));
-		}
-		return (0);
-	}
-	/**
-	 * replace_string - replaces str
-	 * @old: address of old string
-	 * @new: new string
-	 * Return: 1 if replaced, 0 otherwise
-	 */
-	int replace_string(char **old, char *new)
+/**
+ * replace_vars - replaces vars in the tokenized str
+ * @info: the ptr struct
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replace_vars(info_t *info)
+{
+	int l = 0;
+	list_t *node;
+
+	for (l = 0; info->argv[l]; l++)
 	{
-		free(*old);
-		*old = new;
-		return (1);
+		if (info->argv[l][0] != '$' || !info->argv[l][1])
+			continue;
+		if (!_strcmp(info->argv[l], "$?"))
+		{
+			replace_string(&(info->argv[l]), _strdup(convert_number(info->status, 10, 0)));
+			continue;
+		}
+		if (!_strcmp(info->argv[l], "$$"))
+		{
+			replace_string(&(info->argv[l]), _strdup(convert_number(getpid(), 10, 0)));
+			continue;
+		}
+		node = node_starts_with(info->env, &info->argv[l][1], '=');
+		if (node)
+		{
+			replace_string(&(info->argv[l]), _strdup(_strchr(node->str, '=') + 1));
+			continue;
+		}
+		replace_string(&info->argv[l], _strdup(""));
 	}
+	return (0);
+}
+
+/**
+ * replace_string - replaces str
+ * @old: address of old string
+ * @new: new string
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replace_string(char **old, char *new)
+{
+	free(*old);
+	*old = new;
+	return (1);
+}
+
